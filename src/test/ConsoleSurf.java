a18d2bc81ddd4f7e4104dc94c051787fb27a8edf
Allan De Souza Melo <allan_de_souza_melo@hotmail.com>
2020-03-03 04:36:30 -0300

    update code

--- src/test/ConsoleSurf.java
+++ src/test/ConsoleSurf.java
@@ -22,190 +22,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 
  
-public class WebRobot {
-	public static void download(String link,String path) {
-		System.out.println("downloading:"+link);
-		try {
-			BufferedInputStream inputStream = new BufferedInputStream(new URL(link).openStream());
-			System.out.println("download stream opened:"+link);
-			if(!new File(path).exists()) {
-				OutputStream os = new FileOutputStream(new File(path));
-				//Thread.sleep(2000);
-				FileOutputStream fileOS = new FileOutputStream(path); 
-			    byte data[] = new byte[1024];
-			    int byteContent;
-			    System.out.println("writing download file..");
-			    int counter = 0;
-			    while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
-			    	Thread.sleep(2);
-			    	System.err.println(link+" "+(++counter/1024.0)+"/mb");
-			        fileOS.write(data, 0, byteContent);
-			    }
-			    System.out.println("finished : "+link);
-			    fileOS.close();
-			    os.close();
-			    System.gc();
-			}else {
-				System.out.println("skipped file:"+path);
-			}
-		} catch (Exception e) {
-		    // handles IO exceptions
-			e.printStackTrace();
-			System.out.println("download fail");
-		}
-	}
-	public List<String> findTxt(String start,String end,String html){
-		List<String> tag = new ArrayList();
-		try {
-			Pattern pattern = Pattern.compile(start+"(.*?)"+end);
-			//System.out.println("compile<-------------");
-			Matcher matcher = pattern.matcher(html);
-			while(matcher.find()) {
-				Thread.sleep(5);
-				//System.out.println("searching txt..");
-				try {
-					tag.add(matcher.group(1));
-					//System.out.println("found txt:"+matcher.group(1));
-				}catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-			return tag;
-		}catch(Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	public List<String> findVideoLink(String html) {
-		System.out.println("searching video...");
-		List<String> list = findTxt("<video ", "</video>", html);
-		List<String> link = new ArrayList();
-		try {
-			for(String s:list) {
-				Thread.sleep(5);
-				System.out.println("<video "+s+"</video>");
-				for(String ss:findTxt("http", "\"", s)) {
-					Thread.sleep(5);
-					System.out.println("found video : http"+ss);
-					link.add("http"+ss);
-				}
-			}
-			return link;
-		}catch(Exception e) {
-			System.out.println("vazio");
-			return null;
-		}
-	}
-	public List<String> findLinks(String html) {
-		List<String> list = findTxt("href=\"","\"",html);
-		List<String> links = new ArrayList();
-		try {
-			for(String s:list) {
-				Thread.sleep(5);
-				links.add(s);
-			}
-			return links;
-		}catch(Exception e) {
-			return null;
-		}
-	}
-	public void browseAndFindVideo(String url) {
-		System.out.println("current page:"+url);
-		String page = findPage(url);
-		List<String> videosLinks = findVideoLink(page);
-		for(String s:videosLinks) {
-			try {
-				//OutputStream os = null;
-				//os = new FileOutputStream(new File("c:/Users/allan/downloads/os.txt"));
-				//FileInputStream in = new FileInputStream(new File("c:/Users/allan/downloads/os.txt"));
-				//os.write((Files.readAllBytes(Paths.get("c:/Users/allan/downloads/os.txt")).toString()+"\n"+s).getBytes());
-				String fn = s.replaceAll("/", "").replaceAll(":", "");
-				fn = "E:\\robot_data\\"+fn+".mp4";
-				System.out.println(fn);
-				download(s,fn);
-			}catch(Exception e) {
-				
-			}
-		}
-		List<String> links = findLinks(page);
-		Collections.shuffle(links);
-		String prev = "";
-		for(String s:links) {
-			try {
-				if(s.charAt(0) == '/' && s.charAt(1) != '/' && !s.equals(prev)) {
-					prev = s;
-					System.out.println("redirecting to :"+url+s);
-					for(String ss:findVideoLink(findPage(url+s))) {
-						Thread.sleep(5);
-						try {
-							//OutputStream os = null;
-							//os = new FileOutputStream(new File("c:/Users/allan/downloads/os.txt"));
-							//FileInputStream in = new FileInputStream(new File("c:/Users/allan/downloads/os.txt"));
-							//os.write((Files.rea(Paths.get("c:/Users/allan/downloads/os.txt"))+"\n"+ss).getBytes());
-							String fn = ss.replaceAll("/", "").replaceAll(":", "");
-							fn = "E:\\robot_data\\"+fn+".mp4";
-							System.out.println(fn);
-							download(ss,fn);
-						}catch(Exception e) {
-							
-						}
-						Collections.shuffle(links);
-					}
-				}else {
-					if(s.charAt(0) == 'h' && s.charAt(1) == 't' && !s.equals(prev)) {
-						prev = s;
-						System.out.println("redirecting to :"+s);
-						for(String ss:findVideoLink(findPage(s))) {
-							Thread.sleep(5);
-							try {
-								//OutputStream os = null;
-								//os = new FileOutputStream(new File("c:/Users/allan/downloads/os.txt"));
-								//FileInputStream in = new FileInputStream(new File("c:/Users/allan/downloads/os.txt"));
-								//os.write((Files.rea(Paths.get("c:/Users/allan/downloads/os.txt"))+"\n"+ss).getBytes());
-								String fn = ss.replaceAll("/", "").replaceAll(":", "");
-								fn = "E:\\robot_data\\"+fn+".mp4";
-								System.out.println(fn);
-								download(ss,fn);
-							}catch(Exception e) {
-								
-							}
-							Collections.shuffle(links);
-						}
-					}else {
-						System.out.println("skip:"+s);
-					}
-				}
-			}catch(Exception e) {
-				
-			}
-		}
-	}
-	public String findPage(String page) {
-		//change this
-		System.setProperty("webdriver.chrome.driver", "chromedriver\\chromedriver.exe");
-		String html = "";
-		ChromeOptions op = new ChromeOptions();
-		op.setHeadless(true);
-		WebDriver driver = new ChromeDriver(op);
-		driver.get(page);
-		try {
-			Thread.sleep(20000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		}
-		html = driver.getPageSource();
-		
-		driver.close();
-		try {
-			Thread.sleep(5);
-			//Runtime.getRuntime().exec("taskkill /f /im chrome*");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			//e.printStackTrace();
-		}
-		return html;
-	}
+public class ConsoleSurf extends TestChrome {
 	public void openPage(String curPage) {
 		//WebRobot chrome = new WebRobot();
 		//System.out.print("type url:");
@@ -258,7 +75,7 @@ public class WebRobot {
 		}
 	}
 	public static void main(String[]args) {
-		WebRobot chrome = new WebRobot();
+		ConsoleSurf chrome = new ConsoleSurf();
 		List<String> buttons = null;
 		List<String> links = null;
 		String currentPage = null;
@@ -419,7 +236,7 @@ public class WebRobot {
 				commands.add("showVars >> show all stored variables");
 				commands.add("download >> download a file based on url");
 				commands.add("showHtml >> show page html");
-				commands.add("find >> find tags or anything inside html based on start and end strings");
+				commands.add("find >> find tags or anything inside html based on start and end strings delimiters");
 				for(String s:commands) {
 					System.out.println(s);
 				}
